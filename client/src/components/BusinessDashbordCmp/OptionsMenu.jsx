da9e17f9e033d5f9762387c696bcda9730d4659d
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-09-03 12:27:29 -0400

    Create OptionsMenu component

---
+++ client/src/components/BusinessDashbordCmp/OptionsMenu.jsx
@@ -0,0 +1,50 @@
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import ListIcon from "@mui/icons-material/List";
+
+import React from "react";
+
+export default function OptionsMenu({ options, handleSelectionChnage }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <ListIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        {options.map((service, index) => (
+          <MenuItem
+            key={index}
+            onClick={() => {
+              handleSelectionChnage(service);
+              handleClose();
+            }}
+          >
+            {service}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
