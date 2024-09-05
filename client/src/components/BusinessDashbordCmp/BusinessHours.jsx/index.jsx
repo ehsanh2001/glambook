123da18720650f3b0ebe4d6c5f88e4e5397a1907
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-09-05 08:52:45 -0400

    Use Tabs instead of Accordion

--- client/src/components/BusinessDashbordCmp/BusinessHours.jsx/index.jsx
+++ client/src/components/BusinessDashbordCmp/BusinessHours.jsx/index.jsx
@@ -6,44 +6,68 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
 import WeeklyHourSelector from "./WeeklyHourSelector";
-import ClosingDateTime from "./ClosingDateTime";
+import ExceptionalClosures from "./ExceptionalClosures";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export default function BusinessHours({ business, setBusiness }) {
+  const [tab, setTab] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setTab(newValue);
+  };
+
   return (
-    <div>
-      <Accordion defaultExpanded>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="business-hours-content"
-          id="business-hours-header"
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={tab}
+          onChange={handleChange}
+          aria-label="basic tabs example"
         >
-          <Typography variant="h6" gutterBottom>
-            Weekly Business Hours
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <WeeklyHourSelector business={business} setBusiness={setBusiness} />
-        </AccordionDetails>
-        <AccordionActions>
-          <Button>Cancel</Button>
-          <Button>Agree</Button>
-        </AccordionActions>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="exception-closing-dates-content"
-          id="panel2-header"
-        >
-          <Typography variant="h6" gutterBottom>
-            Closing Dates/Time
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <ClosingDateTime business={business} setBusiness={setBusiness} />
-        </AccordionDetails>
-      </Accordion>
-    </div>
+          <Tab label="Weekly Hours" {...a11yProps(0)} />
+          <Tab label="Exceptional Closures" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={tab} index={0}>
+        <WeeklyHourSelector business={business} setBusiness={setBusiness} />
+      </CustomTabPanel>
+      <CustomTabPanel value={tab} index={1}>
+        <ExceptionalClosures business={business} setBusiness={setBusiness} />
+      </CustomTabPanel>
+    </Box>
   );
 }
