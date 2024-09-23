134cbb4fa759a90ba08df503b18bd1f2017b26b6
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-09-23 10:08:18 -0400

    Add 'owenr' refrence to Business document

--- server/models/Business.js
+++ server/models/Business.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const businessSchema = new Schema({
-  // owner: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "User",
-  //   required: true,
-  // },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   businessName: {
     type: String,
     required: true,
